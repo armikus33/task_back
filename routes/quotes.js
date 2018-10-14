b2904835b27d06c3946946dfcf343c0ca58ee705
armikus33 <44140406+armikus33@users.noreply.github.com>
2018-10-14 21:59:11 +0600

    fx

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// Require the controllers WHICH WE DID NOT CREATE YET!!
+
 var quotes_controller = require('../controllers/quotes');
 
-
-// a simple test url to check that all of our files are communicating correctly.
 router.get('/', quotes_controller.test);
 
 router.post('/create', quotes_controller.quotes_create);
